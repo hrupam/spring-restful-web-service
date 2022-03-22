174f7e6fe230ab1ab5b4bb1953fd8c0fa48a0a28
Rupam Hari <hrupam55@gmail.com>
2022-03-22 14:24:45 +0530

    create user post mapping

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,10 +1,11 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,6 +18,13 @@ public class UserResource {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.findAll();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     //retrieve user based on id
