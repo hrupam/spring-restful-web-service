a29ba88090161c4de12f326ac1d9adf867a7b75b
Rupam Hari <hrupam55@gmail.com>
2022-06-16 20:53:40 +0530

    using LocaleContextHolder.getLocale()

--- src/main/java/com/rest/webservices/restfulwebservices/home/HomeResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/home/HomeResource.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 @RequestMapping("/")
@@ -18,7 +15,7 @@ public class HomeResource {
     private MessageSource messageSource;
 
     @GetMapping("")
-    public String helloWorldInternationalised(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    public String helloWorldInternationalised() {
         return messageSource.getMessage("hello.world.message", null, "Language not available", LocaleContextHolder.getLocale());
     }
 }
